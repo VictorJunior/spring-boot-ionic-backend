d29ee930985500713e35c3cf65d86f5fbbe56b82
VictorJunior <victorcastrojunior@yahoo.com.br>
2018-07-12 22:02:02 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/victorjr/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/victorjr/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,26 @@
 package com.victorjr.cursomc.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.victorjr.cursomc.domain.Categoria;
 import com.victorjr.cursomc.domain.Produto;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+	@Transactional(readOnly=true)
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+	
+	//Este tipo de consulta usa o padrão do Spring Data de métodos, muito mais fácil e prático assim. Referência: https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
+	//Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 
 }
