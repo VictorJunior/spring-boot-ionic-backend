aec56a9e86792e3051ee9f8d2e2c1b5e563d2a71
VictorJunior <victorcastrojunior@yahoo.com.br>
2018-07-19 21:58:49 -0300

    Restricao de conteudo: cliente so recupera ele mesmo

--- src/main/java/com/victorjr/cursomc/services/ClienteService.java
+++ src/main/java/com/victorjr/cursomc/services/ClienteService.java
@@ -15,27 +15,35 @@ import org.springframework.transaction.annotation.Transactional;
 import com.victorjr.cursomc.domain.Cidade;
 import com.victorjr.cursomc.domain.Cliente;
 import com.victorjr.cursomc.domain.Endereco;
+import com.victorjr.cursomc.domain.enums.Perfil;
 import com.victorjr.cursomc.domain.enums.TipoCliente;
 import com.victorjr.cursomc.dto.ClienteDTO;
 import com.victorjr.cursomc.dto.ClienteNewDTO;
 import com.victorjr.cursomc.repositories.ClienteRepository;
 import com.victorjr.cursomc.repositories.EnderecoRepository;
+import com.victorjr.cursomc.security.UserSS;
+import com.victorjr.cursomc.services.exceptions.AuthorizationException;
 import com.victorjr.cursomc.services.exceptions.DataIntegrityException;
 import com.victorjr.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	private BCryptPasswordEncoder pe;
-	
+
 	@Autowired
 	private ClienteRepository repo;
-	
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
 	public Cliente find(Integer id) {
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
@@ -47,20 +55,19 @@ public class ClienteService {
 		enderecoRepository.saveAll(obj.getEnderecos());
 		return obj;
 	}
-	
+
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
 		return repo.save(newObj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
-		
+
 		try {
 			repo.deleteById(id);
-		}
-		catch(DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
 		}
 	}
@@ -68,7 +75,7 @@ public class ClienteService {
 	public List<Cliente> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
@@ -77,21 +84,23 @@ public class ClienteService {
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null, null);
 	}
-	
+
 	public Cliente fromDTO(ClienteNewDTO objDto) {
-		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));
 		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
-		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
-		
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+
 		cli.getEnderecos().add(end);
 		cli.getTelefones().add(objDto.getTelefone1());
-		if(objDto.getTelefone2() != null) {
+		if (objDto.getTelefone2() != null) {
 			cli.getTelefones().add(objDto.getTelefone2());
 		}
-		if(objDto.getTelefone3() != null) {
+		if (objDto.getTelefone3() != null) {
 			cli.getTelefones().add(objDto.getTelefone3());
 		}
-		
+
 		return cli;
 	}
 
@@ -99,5 +108,5 @@ public class ClienteService {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
-		
+
 }
---
+++ src/main/java/com/victorjr/cursomc/services/UserService.java
@@ -0,0 +1,17 @@
+package com.victorjr.cursomc.services;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.victorjr.cursomc.security.UserSS;
+
+public class UserService {
+	
+	public static UserSS authenticated() {
+		try {
+			return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		}
+		catch (Exception e) {
+			return null;
+		}
+	}
+}
