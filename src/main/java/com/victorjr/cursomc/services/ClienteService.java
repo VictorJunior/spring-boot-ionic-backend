1b710f04dd2c2e2844193c8c2103d8f988013f4f
VictorJunior <victorcastrojunior@yahoo.com.br>
2018-08-13 21:16:27 -0300

    Enviando imagem via endpoint

--- src/main/java/com/victorjr/cursomc/services/ClienteService.java
+++ src/main/java/com/victorjr/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.victorjr.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.victorjr.cursomc.domain.Cidade;
 import com.victorjr.cursomc.domain.Cliente;
@@ -38,6 +40,9 @@ public class ClienteService {
 	@Autowired
 	private EnderecoRepository enderecoRepository;
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
 		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
@@ -109,4 +114,8 @@ public class ClienteService {
 		newObj.setEmail(obj.getEmail());
 	}
 
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
+	}
+	
 }
